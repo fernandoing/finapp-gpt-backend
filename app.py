a63c3af613a771f4b74584ba2285da31e441ce14
Ernesto Espinoza <ernesto_espinoza@epam.com>
2024-03-21 10:38:46 -0700

    Prompts adjustments | Requesting categories and adding chat history

--- app.py
+++ app.py
@@ -12,15 +12,18 @@ load_dotenv()
 
 client = OpenAI(api_key=os.getenv('OPENAI_API_KEY'))
 EXTERNAL_API_URL = os.getenv('EXTERNAL_API_URL')
+HEADERS = {"Authorization": os.getenv('AUTHORIZATION')}
 
 app = Flask(__name__)
 CORS(app)
 
-def chat_with_gpt_for_expenses(user_input):
-    prompt = prompts.add_expense.format(user_input=user_input,json_example=prompts.json_example)
+
+def chat_with_gpt_for_expenses(user_input, categories):
+    current_date = datetime.now().strftime("%Y-%m-%d")
+    prompt = prompts.add_expense.format(
+        user_input=user_input, json_example=prompts.json_example, categories=categories, current_date=current_date)
     response = chat_with_gpt(prompt)
     try:
-        # Assuming GPT might structure the output correctly based on the prompt's guidance
         expense_details = json.loads(response)
         if 'expense_name' in expense_details:
             return expense_details
@@ -29,56 +32,122 @@ def chat_with_gpt_for_expenses(user_input):
     except json.JSONDecodeError:
         return {}
 
+
 def add_expense(expense_data):
-    response = requests.post(f"{EXTERNAL_API_URL}/expenses", json={"expense": expense_data})
-    if response.status_code != 200:
+    response = requests.post(
+        f"{EXTERNAL_API_URL}/expenses", json=expense_data, headers=HEADERS)
+    print(response)
+    if response.ok:
+        return response.json()
+    else:
         return {"error": f"API call failed with status code {response.status_code}", "details": response.text}
-    return response.json()
+
+
+def get_expenses():
+    response = requests.get(f"{EXTERNAL_API_URL}/expenses", headers=HEADERS)
+    if response.ok:
+        return response.json()
+    else:
+        return {"error": f"API call failed with status code {response.status_code}", "details": response.text}
+
+
+def get_categories():
+    response = requests.get(
+        f"{EXTERNAL_API_URL}/expense_categories", headers=HEADERS)
+    if response.ok:
+        return response.json()
+    else:
+        return {"error": f"API call failed with status code {response.status_code}", "details": response.text}
+
+
+def get_history():
+    response = requests.get(
+        f"{EXTERNAL_API_URL}/chat_history", headers=HEADERS)
+    if response.ok:
+        return response.json()
+    else:
+        return {"error": f"API call failed with status code {response.status_code}", "details": response.text}
+
+
+def add_history(input):
+    response = requests.post(
+        f"{EXTERNAL_API_URL}/chat_history", json=input, headers=HEADERS)
+    if response.ok:
+        return response.json()
+    else:
+        return {"error": f"API call failed with status code {response.status_code}", "details": response.text}
+
+
+
+
+
+
+
 
 @app.route('/get_response', methods=['POST'])
 def get_response():
     data = request.get_json()
     user_input = data['user_input']
-
-    # First, let's use GPT to understand if this is an expense-related query or general financial advice.
-    # This example assumes you have trained or fine-tuned GPT to respond accordingly.
+    add_history({"role": 1, "message": user_input})
 
     prompt = prompts.determine_intent.format(user_input=user_input)
-    test = prompts.expense_added.format(user_input=user_input)
     intent = chat_with_gpt(prompt)
+    print(intent)
+    response = ""
     if "1" in intent:
-        # Specific trigger or keyword based on GPT's response indicating an expense is to be added.
-        expense_details = chat_with_gpt_for_expenses(user_input)
+        categories = get_categories()
+        expense_details = chat_with_gpt_for_expenses(user_input, categories)
         if expense_details:
+            print(expense_details)
             expense_response = add_expense(expense_details)
+            print(expense_response)
             prompt = prompts.expense_added.format(user_input=expense_details)
             print(prompt)
             if not "error" in expense_response:
                 response = chat_with_gpt(prompt)
-                return jsonify({'response': response})
-            response = chat_with_gpt(prompts.expense_added)
-            return jsonify({'response': response})
+            else:
+                return jsonify({'response': "There was an error adding your expenses. Try again later."})
         else:
             response = chat_with_gpt(prompts.unclear_expense)
-            return jsonify({'response': response})
+    elif "2" in intent:
+        expenses = get_expenses()
+        prompt = prompts.structure_expenses.format(
+            user_input=user_input, expenses=expenses)
+        print(prompt)
+        if not "error" in expenses:
+            response = chat_with_gpt(prompt)
+        else:
+            return jsonify({'response': "There was an error retrieving your expenses. Try again later."})
     else:
-        # For all other inquiries, return a general response.
-        # This could include financial advice or answers to specific questions.
-        general_response = chat_with_gpt(user_input)
-        return jsonify({'response': general_response})
-    
+        response = chat_with_gpt(user_input)
+    add_history({"role": 2, "message": response})
+    print(response)
+    return jsonify({'response': response})
+
+
 def chat_with_gpt(prompt, model="gpt-3.5-turbo"):
+    # previous_messages = get_history().get('chat_history')
+    # print(previous_messages)
+    previous_messages = []
+    if previous_messages is None:
+        previous_messages = []
+
+    messages = [
+        {"role": "system", "content": prompts.base_prompt}
+    ] + previous_messages + [
+        {"role": "user", "content": prompt}
+    ]
     try:
         response = client.chat.completions.create(
             model=model,
-            messages=[
-                {"role": "system", "content": prompts.base_prompt},
-                {"role": "user", "content": prompt}
-            ]
+            messages=messages
+
         )
+        print(response)
         return response.choices[0].message.content.strip()
     except Exception as e:
         print(e)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- prompts.py
+++ prompts.py
@@ -10,25 +10,27 @@ json_example = """
 base_prompt = """
 You are a helpful AI Financial Advisor/Assistant. Remember this app is intended so a user can create a budget and add their expenses via this chat interaction. Be very professional.
 """
-add_expense = 'A user says: {user_input}. If the user wants to add an expense, extract the relevant details such as expense amount, expense name, category id and the date. If user does not specify a date assign current date. The category IDs are: 1 for Groceries, 2 for Rent, 3 for Utilities, 4 for Transportation. Your response must be a json in this format: {json_example}'
+add_expense = 'A user says: {user_input}. If the user wants to add an expense, extract the relevant details such as expense amount, expense name, category id and the date. If user does not specify a date assign current date, here it is: {current_date}. If user inputs a date such as yesterday or last month, use the current date info as reference. The category IDs are in this json: {categories}. Determine the best category for the expense. Your response must be a json in this format: {json_example}. Return the json using double quotes for the keys and the values.'
 determine_intent = """
-    Given the following user input, categorize the intention as either 'Adding an Expense' or 'General/financial purpose talk': 
+    Given the following user input, categorize the intention as either '1. Adding an Expense', '2. View/See Expenses' or '3. General/financial purpose talk': 
 
     User Input: '{user_input}'
 
     Categories:
     1. Adding an Expense
-    2. General/financial purpose talk
+    2. View/See Expenses
+    3. General/financial purpose talk
 
-    Determine the category based on keywords and the structure of the message. If the input contains financial amounts, specific cost-related terms (e.g., 'spent', 'buy', 'cost'), or direct mentions of budgeting items, categorize it as 'Adding an Expense'. If the input discusses financial concepts, asks for advice, mentions planning without specific transaction details, or just want to talk about other topics categorize it as 'General/financial purpose talk'.
+    Determine the category based on keywords and the structure of the message. If the input contains financial amounts, specific cost-related terms (e.g., 'spent', 'buy', 'cost'), or direct mentions of budgeting items, categorize it as '1. Adding an Expense'. If the input intends to review, see or retrieve the expenses, categorize it as '2. View/See Expenses'. If the input discusses financial concepts, asks for advice, mentions planning without specific transaction details, or just want to talk about other topics categorize it as '3. General/financial purpose talk'.
     Respond with the category number ONLY.
 """
 expense_added = """
-    Given the user has just added an expense to their budget, craft a positive and encouraging response informing them of the successful action. The response should make the user feel confident and in control of their financial journey. Include a suggestion for their next possible action to keep them engaged.
-    Here are the details of the expense added: '{user_input}'
+    Given the user has just added an expense to their budget, craft a response informing them of the successful action. The response should have criteria of a professional finances advisor to let the user know if this exepnse was a correct decision or no. Make it concise. Include a suggestion for their next possible action to keep them engaged.
+    Here are the details of the expense added: '{user_input}'. Use them to craft the response.
     Example of a good response:
-    Fantastic! Your [expense name] of [expense amount] on [expense category] has been successfully added to your budget for [month]. Your diligence in tracking your expenses is key to achieving your financial goals. Would you like to review your current budget summary or add another expense?
+    Fantastic! Your expense has been successfully added to your budget for. Would you like to review your current budget summary or add another expense?
 """
 unclear_expense = """
 It seems like the user wanted to add an expense to their budget but they're not providing the required information or maybe you though they wanted to add an expense and confused the intent with that. Give me a response to let them know you need more information such as the expense name and the amount they spent on it if they want to add an expense so they can try again.
 """
+structure_expenses = 'Given the user wants to see their expenses, this is the user input: {user_input} return a formatted response with the list of expenses and its information. Do not respond with IDs. Here is the information with the expenses: {expenses}. On the date field, just show month and year, not day. This has to be a response for the final user.'
